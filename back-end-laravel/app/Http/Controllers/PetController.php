965359ac1605dd52f48bafdf33e233e94d4c870a
SamuelCaitano <smlcaitano@gmail.com>
2022-06-07 23:29:16 -0300

    listagem e cadastro de pets

--- back-end-laravel/app/Http/Controllers/PetController.php
+++ back-end-laravel/app/Http/Controllers/PetController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PetModel;
-use Illuminate\Http\Request;
+use App\Http\Requests\PetRequest;
+use App\Models\PetModel; 
 
 class PetController extends Controller
 {
-    public function index()
-    {
-         return PetModel::get();
-    }
+  /**
+   * Retorna a lista de pets cadastrados
+   * 
+   * @return Collection
+   */
+  public function index()
+  {
+    return PetModel::get();
+  }
+
+  /**
+   * Cria um pet no banco de dados
+   * 
+   * @param PetRequest $request
+   * @return Pet
+   */
+  public function store(PetRequest $request)
+  {
+    $data = $request->all();
+
+    return PetModel::create($data);
+  }
 }
